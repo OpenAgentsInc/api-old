ef21bc00b9eb62fd81e99a5e681b44f62f666951
Christopher David <chris@arcadelabs.co>
2023-07-03 19:43:59 -0400

    and now try extracting message

--- api/conversations.py
+++ api/conversations.py
@@ -5,12 +5,13 @@ from flask import jsonify, request
 def new_message():
     """Handle sending a new message"""
     # Get the message from the request body
-    # data = request.json
+    data = request.json
+
     # conversationId = data.get('conversationId')
     # conversationType = data.get('conversationType')
-    # message = data.get('message')
+    message = data.get('message')
     # userId = data.get('userId')
     # plan = data.get('plan')
 
-    return jsonify({"success": True, "response": "heyyyy"}), 200
-    # return jsonify({"success": True, "response": "sending " + message}), 200
+    # return jsonify({"success": True, "response": "heyyyy"}), 200
+    return jsonify({"success": True, "response": "sending " + message}), 200
